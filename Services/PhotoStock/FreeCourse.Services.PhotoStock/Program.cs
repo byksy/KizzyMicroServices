f4937d21ebeaae8e0d2f0f4d96410f6bcc4ec209
mb <mustafa.buyuksoy@navlungo.com>
2023-06-21 08:46:00 +0300

    Add some config parameter

--- Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
+++ Services/PhotoStock/FreeCourse.Services.PhotoStock/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter());
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_photo_stock";
+    options.RequireHttpsMetadata = false;
+});
 
 var app = builder.Build();
 
@@ -23,5 +35,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseAuthentication();
 
 app.Run();
