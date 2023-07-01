818d9b8d8d6475f57662faed5af0208e098f4db6
mb <mustafa.buyuksoy@navlungo.com>
2023-07-01 14:59:56 +0300

    Add discount model

---
+++ Services/Discount/FreeCourse.Services.Discount/Models/Discount.cs
@@ -0,0 +1,10 @@
+namespace FreeCourse.Services.Discount.Models;
+[Dapper.Contrib.Extensions.Table("discount")]
+public class Discount
+{
+    public int Id { get; set; }
+    public string UserId { get; set; } = null!;
+    public int Rate { get; set; }
+    public string Code { get; set; } = null!;
+    public DateTime CreatedTime { get; set; }
+}
