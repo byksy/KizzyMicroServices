f530d6b1839951b341b77b254e82ec87f7e66a47
mb <mustafa.buyuksoy@navlungo.com>
2023-07-12 17:39:33 +0300

    Add Dtos

--- Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -7,7 +7,7 @@ public class OrderItem : Entity
     public string ProductId { get; private set; }
     public string ProductName { get; private set; }
     public string PictureUrl { get; private set; }
-    public Decimal Price { get; private set; }
+    public decimal Price { get; private set; }
 
     public OrderItem(string productId, string productName, string pictureUrl, decimal price)
     {
